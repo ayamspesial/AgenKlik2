a91dfddb1aaba692cd5c9dff95679d66fb73b253
ayamspesial <49014214+ayamspesial@users.noreply.github.com>
2024-01-09 23:29:11 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-    <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
