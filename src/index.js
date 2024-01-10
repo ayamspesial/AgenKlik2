20347592424c65320827de2709f32de8880a2b36
Russell <russellfebrisa@gmail.com>
2024-01-10 22:47:15 +0700

    Trying again

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        
         <HashRouter>
             <App />
         </HashRouter>
